d3ad9447853a832d05531b1e24ebda14bf4edf90
Pablo db <pablo.dominguezblanco@gmail.com>
2018-07-16 18:51:37 +0200

    Initial express server with route files

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+// Users are able to create posts (comments)
+
+const express = require('express');
+const router = express.Router();
+
+// @route 	GET api/posts/test
+// @desc 	Tests post route
+// @access	Public
+router.get('/test', (req, res) => res.json({msg: 'Posts works'}));
+
+module.exports = router
